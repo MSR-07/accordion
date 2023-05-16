299f4ec0ccff3f62cf72963847ee304a83dc096d
MSR-07 <102695412+MSR-07@users.noreply.github.com>
2023-05-16 15:41:04 +0500

    Add Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './App.css';
+import Question from './components/Question'
+import question from "./data/questions"
 
 function App() {
+  const [questions, setQuestions] = useState(question)
   return (
    <>
+   <main className='flex justify-center items-start min-vh-100'>
+    <div className='w-full my-20 mx-auto py-10 px-8 grid gap-x-4 gap-y-8 max-w-4xl rounded bg-white sm:grid-cols-[180px_minmax(100px,_1fr)] '>
+      <h3 className='leading-7 font-medium'>question and answer about login</h3>
+      <section className='my-0 mx-auto max-w-6xl'>
+      {questions.map((question)=>{
+        return <Question key={question.id} {...question}/>
+      })}
+
+      </section>
+    </div>
+   </main>
 
    </>
   );
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import{AiOutlineMinus, AiOutlinePlus} from "react-icons/ai"
 
-const Question = () => {
+
+const Question = ({title, info}) => {
+    const[showinfo, setShowinfo] = useState(false);
+    
     return (
-        <div>
-            
-        </div>
+        <article className='question py-4 px-6 mb-4 border-s-stone-700 shadow-lg'>
+    
+            <header className='flex justify-between items-center' >
+                <h4 className='mb-0 transform-none leading-6'>{title}</h4>
+      
+                <button 
+                className='border-transparent w-8 h-8 flex items-center justify-center rounded-full cursor-pointer align-self-center ml-4 min-w-[32px] bg-slate-200 text-rose-500'
+                onClick={()=>{setShowinfo(!showinfo)}} >
+                {showinfo? <AiOutlineMinus/> :<AiOutlinePlus/>}
+                </button>
+            </header>                                                                                                                                                                                                                  
+                                                                                                                
+            {showinfo && <p className='mb-0 mt-2 text-gray-400'>{info}</p>}
+        </article>
     );
 }
 
